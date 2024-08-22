25f9ae24b9f981955e1101cb154ff8ad1088aecc
h.sb <jksg01019@gmail.com>
2024-08-22 16:47:50 +0900

    test

--- grounded_sam2_hf_model_demo.py
+++ grounded_sam2_hf_model_demo.py
@@ -7,7 +7,7 @@ from utils.supervision_utils import CUSTOM_COLOR_MAP
 from PIL import Image
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
-from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection 
+from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
 
 # environment settings
 # use bfloat16
@@ -28,13 +28,14 @@ sam2_predictor = SAM2ImagePredictor(sam2_model)
 model_id = "IDEA-Research/grounding-dino-tiny"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 processor = AutoProcessor.from_pretrained(model_id)
-grounding_model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
-
+grounding_model = AutoModelForZeroShotObjectDetection.from_pretrained(
+    model_id).to(device)
 
 # setup the input image and text prompt for SAM 2 and Grounding DINO
 # VERY important: text queries need to be lowercased + end with a dot
-text = "car. tire."
-img_path = 'notebooks/images/truck.jpg'
+#text = "car. tire."
+text = "soccer field. field markings."
+img_path = 'left_frames/0.jpg'
 
 image = Image.open(img_path)
 
@@ -49,9 +50,7 @@ results = processor.post_process_grounded_object_detection(
     inputs.input_ids,
     box_threshold=0.4,
     text_threshold=0.3,
-    target_sizes=[image.size[::-1]]
-)
-
+    target_sizes=[image.size[::-1]])
 """
 Results is a list of dict with the following structure:
 [
@@ -75,8 +74,6 @@ masks, scores, logits = sam2_predictor.predict(
     box=input_boxes,
     multimask_output=False,
 )
-
-
 """
 Post-process the output of the model to get the masks, scores, and logits for visualization
 """
@@ -84,17 +81,14 @@ Post-process the output of the model to get the masks, scores, and logits for vi
 if masks.ndim == 4:
     masks = masks.squeeze(1)
 
-
 confidences = results[0]["scores"].cpu().numpy().tolist()
 class_names = results[0]["labels"]
 class_ids = np.array(list(range(len(class_names))))
 
 labels = [
     f"{class_name} {confidence:.2f}"
-    for class_name, confidence
-    in zip(class_names, confidences)
+    for class_name, confidence in zip(class_names, confidences)
 ]
-
 """
 Visualize image with supervision useful API
 """
@@ -102,20 +96,23 @@ img = cv2.imread(img_path)
 detections = sv.Detections(
     xyxy=input_boxes,  # (n, 4)
     mask=masks.astype(bool),  # (n, h, w)
-    class_id=class_ids
-)
-
+    class_id=class_ids)
 """
 Note that if you want to use default color map,
 you can set color=ColorPalette.DEFAULT
 """
 box_annotator = sv.BoxAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = box_annotator.annotate(scene=img.copy(), detections=detections)
+annotated_frame = box_annotator.annotate(scene=img.copy(),
+                                         detections=detections)
 
-label_annotator = sv.LabelAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
+label_annotator = sv.LabelAnnotator(
+    color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+annotated_frame = label_annotator.annotate(scene=annotated_frame,
+                                           detections=detections,
+                                           labels=labels)
 cv2.imwrite("groundingdino_annotated_image.jpg", annotated_frame)
 
 mask_annotator = sv.MaskAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = mask_annotator.annotate(scene=annotated_frame, detections=detections)
+annotated_frame = mask_annotator.annotate(scene=annotated_frame,
+                                          detections=detections)
 cv2.imwrite("grounded_sam2_annotated_image_with_mask.jpg", annotated_frame)
